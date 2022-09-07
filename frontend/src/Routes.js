df16719a581a39e1843ca8b95ae59a4565178772
황성욱 <sunguk0810@gmail.com>
2022-09-07 14:11:55 +0900

    Display a note - Render the note form

--- frontend/src/Routes.js
+++ frontend/src/Routes.js
@@ -2,13 +2,19 @@ import React from "react";
 import { Route, Routes } from "react-router-dom";
 import Home from "./containers/Home";
 import Login from "./containers/Login";
+import NewNote from "./containers/NewNote";
+import Notes from "./containers/Notes";
 import NotFound from "./containers/NotFound";
+import Signup from "./containers/Signup";
 
 export default function Links() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/login" element={<Login />} />
+      <Route path="/signup" element={<Signup />} />
+      <Route path="/notes/new" element={<NewNote />} />
+      <Route path="/notes/:id" element={<Notes />} />
       {/* 
         Finally, catch all unmatched routes
       */}
