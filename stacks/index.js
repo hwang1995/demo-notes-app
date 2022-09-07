6b31f156b8d12302a1a0412e52e0edf70e3739dc
황성욱 <sunguk0810@gmail.com>
2022-09-07 10:44:09 +0900

    Setting up our React app

--- stacks/index.js
+++ stacks/index.js
@@ -1,6 +1,7 @@
 import { App } from "@serverless-stack/resources";
 import { ApiStack } from "./ApiStack";
 import { AuthStack } from "./AuthStack";
+import { FrontendStack } from "./FrontendStack";
 import { StorageStack } from "./StorageStack";
 
 /**
@@ -14,6 +15,6 @@ export default function (app) {
       format: "esm",
     },
   });
-  app.stack(StorageStack).stack(ApiStack).stack(AuthStack);
+  app.stack(StorageStack).stack(ApiStack).stack(AuthStack).stack(FrontendStack);
   // app.stack(MyStack);
 }
